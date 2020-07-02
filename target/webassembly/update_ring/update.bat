4ccc4d77ce5217f037fbd9e725c0e82f0e6aeff6
Mahmoud Fayed <msfclipper@yahoo.com>
2020-07-03 02:07:00 +0300

    Add target/webassembly/update_ring

---
+++ target/webassembly/update_ring/update.bat
@@ -0,0 +1,13 @@
+rem	Simple Batch file to update Ring and RingQt in the WebAssembly-Qt Version 
+rem	Author : Mahmoud Fayed <msfclipper@yahoo.com>
+
+copy b:\ring\src\*.c ..\qtproject\
+copy ring_ext.c ..\qtproject\
+copy b:\ring\include\*.h ..\qtproject\
+copy ring_ext.h ..\qtproject\
+cd b:\ring\extensions\ringqt\
+call gencodewebassembly.bat
+copy cpp\src\*.cpp b:\pwct2\target\webassembly\qtproject\
+copy cpp\include\*.h b:\pwct2\target\webassembly\qtproject\
+call gencodeqt512_nobluetooth.bat
+cd b:\pwct2\target\webassembly\update_ring
