30dc38f90474d66c9e7a454dcbf32e8999748eaa
Mahmoud Fayed <msfclipper@yahoo.com>
2021-02-02 13:51:42 +0300

    Update Batch File: updatewebasmapp.bat

--- updatewebasmapp.bat
+++ updatewebasmapp.bat
@@ -2,8 +2,8 @@ echo Generate the Ring Object File
 ring pwct.ring -go -norun
 echo Copy files to target\webassembly\qtproject folder
 copy *.ringo target\webassembly\qtproject
-copy /Y mobileapp\*.ring target\webassembly\qtproject\mobileapp
-copy /Y project.qrc target\webassembly\qtproject\mobileapp
+rem copy /Y mobileapp\*.ring target\webassembly\qtproject\mobileapp
+rem copy /Y project.qrc target\webassembly\qtproject\mobileapp
 echo Delete temp. files
 del *.ringo
 echo Done!
