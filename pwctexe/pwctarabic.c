87baf219547f0e8a7f2b98766b1b2487b7857742
Mahmoud Fayed <msfclipper@yahoo.com>
2022-02-12 17:34:27 +0300

    Update PWCT

--- pwctexe/pwctarabic.c
+++ pwctexe/pwctarabic.c
@@ -5,6 +5,6 @@
 #include "conio.h"
 int WINAPI WinMain ( HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd )
 {
-	ShellExecuteA(NULL, "open", "ringw.exe", "pwct.ring arabic", NULL, 1);
+	ShellExecuteA(NULL, "open", "ringw.exe", "pwct.ringo arabic", NULL, 1);
 	return 0;
 }
