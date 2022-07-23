b56d9c4434b0d4d0759ada0859760803e06b32a3
Mahmoud Fayed <msfclipper@yahoo.com>
2022-07-23 07:56:55 +0300

    Update PWCT For WebAssembly

--- target/webassembly/update_ring/updateringonly.bat
+++ target/webassembly/update_ring/updateringonly.bat
@@ -2,6 +2,6 @@ rem	Simple Batch file to update Ring only WebAssembly-Qt Version
 rem	Author : Mahmoud Fayed <msfclipper@yahoo.com>
 
 copy b:\ring\language\src\*.c ..\qtproject\ring\src\
-copy ring_ext.c ..\qtproject\ring\src\
+copy ext.c ..\qtproject\ring\src\
 copy b:\ring\language\include\*.h ..\qtproject\ring\include\
-copy ring_ext.h ..\qtproject\ring\include\
+copy ext.h ..\qtproject\ring\include\
