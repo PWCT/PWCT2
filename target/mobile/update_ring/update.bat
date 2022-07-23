3350c137763674ea59ac59ba986303608514aa35
Mahmoud Fayed <msfclipper@yahoo.com>
2022-07-23 07:34:01 +0300

    Update PWCT For Mobile

--- target/mobile/update_ring/update.bat
+++ target/mobile/update_ring/update.bat
@@ -2,9 +2,9 @@ rem	Simple Batch file to update Ring and RingQt in the Android-Qt Version
 rem	Author : Mahmoud Fayed <msfclipper@yahoo.com>
 
 copy b:\ring\language\src\*.c ..\qtproject\ring\src\
-copy ring_ext.c ..\qtproject\ring\src\
+copy ext.c ..\qtproject\ring\src\
 copy b:\ring\language\include\*.h ..\qtproject\ring\include\
-copy ring_ext.h ..\qtproject\ring\include\
+copy ext.h ..\qtproject\ring\include\
 cd ..\..\..\..\ring\extensions\ringqt\
 call gencodeandroid.bat
 copy cpp\src\*.cpp b:\pwct2\target\mobile\qtproject\ringqt\src\
