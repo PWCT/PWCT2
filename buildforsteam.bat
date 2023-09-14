0d6aed805aaba760b79f648dbd9d06c62c992a85
Mahmoud Fayed <msfclipper@yahoo.com>
2023-09-14 12:58:03 +0300

    Update buildforsteam.bat

--- buildforsteam.bat
+++ buildforsteam.bat
@@ -1,2 +1,2 @@
-B:\RingWork\Releases\Ring118\ring\bin\ring pwct.ring -go -norun
+c:\pl\Ring18\bin\ring pwct.ring -go -norun
 copy pwct.ringo "B:\pwct2\UploadingPWCTonSteam\steamworks_sdk_153a\sdk\tools\ContentBuilder\content\PWCT\pwct.ringo"
